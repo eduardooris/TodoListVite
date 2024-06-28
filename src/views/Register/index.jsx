27d1c0aeb87fb52da5850d38cfa916a8b4bdc4f4
Mauricio Xavier <mauricioxavier907@gmail.com>
2024-06-28 20:11:23 -0300

    criado os botões de input e tela de registro

--- src/views/Register/index.jsx
+++ src/views/Register/index.jsx
@@ -2,15 +2,20 @@ import Banner from '../../assets/banner.svg'
 import Container from '../../components/container'
 import Style from './register.module.css'
 import Button from '../../components/button'
+import Input from '../../components/input'
 
 function Register() {
     return (
-        <div className={Style.body}>
+        <div className={Style.body}>    
             <Container><img className={Style.img} src={Banner} alt="My Icon" /></Container>
             <div className={Style.divCon}>
                 <Container><div className={Style.containerprimary}>
-                    <h2></h2>
-                    
+                    <h1 className={Style.reg}>Registre-se</h1>
+                    <Input style={{marginBottom: '15px'}} placeholder={'Primeiro Nome'}> </Input>
+                    <Input style={{marginBottom: '15px'}} placeholder={'Sobrenome'}></Input>
+                    <Input style={{marginBottom: '15px'}} placeholder={'E-mail'}></Input>
+                    <Input style={{marginBottom: '15px'}} placeholder={'Senha'}></Input>
+                    <Input style={{marginBottom: '30px'}} placeholder={'Confirmar Senha'}></Input>
                     <Button>Começar</Button>
                 </div></Container>
             </div>
